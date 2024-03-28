7cb25929c4dac5ade0f88fe0295993514ba185ea
Nikolay Grishuck <dioshoc@gmail.com>
2024-03-28 10:46:38 +0300

    Add alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
-
+ 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find: '@app', replacement: path.resolve(__dirname, 'src/app')},
+      {find: '@shared', replacement: path.resolve(__dirname, 'src/shared')},
+      {find: '@assets', replacement: path.resolve(__dirname, 'src/shared/assets')}
+    ],
+  },
 })

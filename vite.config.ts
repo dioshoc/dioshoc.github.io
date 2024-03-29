bf43701041ee216ee0f48281623964ef60dbe56f
Nikolay Grishuck <dioshoc@gmail.com>
2024-03-29 18:50:10 +0300

    fix fonts, add alias, refactor WalletIcon.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import {defineConfig} from 'vite'
-import * as path from 'path'
-import react from '@vitejs/plugin-react'
- 
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      {find: '@app', replacement: path.resolve(__dirname, 'src/app')},
-      {find: '@shared', replacement: path.resolve(__dirname, 'src/shared')},
-      {find: '@assets', replacement: path.resolve(__dirname, 'src/shared/assets')}
+      { find: '@app', replacement: path.resolve(__dirname, 'src/app') },
+      { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
+      {
+        find: '@assets',
+        replacement: path.resolve(__dirname, 'src/shared/assets'),
+      },
+      {
+        find: '@entities',
+        replacement: path.resolve(__dirname, 'src/entities'),
+      },
     ],
   },
-})
+});

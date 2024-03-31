69f7a9d25a9047b4f073aaf818cf5fe2d825b1ac
Nikolay Grishuck <dioshoc@gmail.com>
2024-03-31 20:11:56 +0300

    Rename folder

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import Container from '@shared/ui/Container';
-import MainAbout from './section/MainAbout';
-import MainBanner from './section/MainBanner';
-import MainOffers from './section/MainOffers';
+import MainAbout from './sections/MainAbout';
+import MainBanner from './sections/MainBanner';
+import MainOffers from './sections/MainOffers';
 import styles from './styles.module.scss';
 
 function MainPage() {
